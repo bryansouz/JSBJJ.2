475fdcdcdd49f2b8d93846ea9ca134ad19c80c00
bryansouz <bryan.soares19@hotmail.com>
2023-03-05 21:42:11 -0300

    toggle.menu

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu');
-
-
-console.log(menu);
+const navMenu = document.getElementById('nav-menu-id');
+const menuItems = document.querySelectorAll('.nav-item');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-    
+    navMenu.classList.toggle('ativo');
+});
 
-})
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    menu.classList.remove('ativo');
+    navMenu.classList.remove('ativo');
+  });
+});
